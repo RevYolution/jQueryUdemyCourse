c07aae3e9362ee2a0803d5cfa2caa2637028f2de
Jon Rice <revyolution1120@gmail.com>
2020-03-13 11:51:59 -0700

    Setting up base for effects and animations for jQuery class.

---
+++ effect_animations/js/script.js
@@ -0,0 +1,7 @@
+// $(function(){}) is the shorthand notation for jQuery(function(){}) they work the same but industry convention is to use $. 
+$(function() {
+  // jQuery goes here...
+
+  // Uncomment this line to fade out the red box on page load
+  // $(".red-box").fadeOut(2000);
+});
